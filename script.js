0552c0858afa77ae1757c66bd65c1a9fca041585
himalikaasatyarthi <himalikasatyarthi@gmail.com>
2026-07-14 14:36:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -190,6 +190,7 @@ showPage();
 
 const music = document.getElementById("bgMusic");
 const musicBtn = document.getElementById("musicBtn");
+music.volume = 0.2;
 
 musicBtn.addEventListener("click", () => {
     if (music.paused) {
