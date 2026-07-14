16306ef31dea73b771fe0f351016b77a3978c23c
himalikaasatyarthi <himalikasatyarthi@gmail.com>
2026-07-14 16:12:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -140,12 +140,13 @@ text:`
         <div class="photo-grid">
 
             <img src="images/photo3.jpeg">
-            <img src="images/photo4.jpeg">
+            <img src="images/photo17.jpeg">
             <img src="images/photo5.jpeg">
 
             <img src="images/photo13.jpeg">
             <img src="images/photo16.jpeg">
-            <img src="images/photo17.jpeg">
+            <img src="images/photo4.jpeg">
+           
 
         </div>
 
